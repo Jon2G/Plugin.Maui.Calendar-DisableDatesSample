d25391905d34f197acb8b9f5cd14048b1c37717c
Jon2G <jonyjouna@gmail.com>
2024-09-23 19:56:11 -0600

    Initial commit

---
+++ src/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿namespace DisableDatesSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            MyCalendar.DisabledDates = [
+                new DateTime(2024,09,24),
+                new DateTime(2024,09,30),
+                new DateTime(2024,10,01)
+            ];
+
+
+        }
+    }
+
+}
